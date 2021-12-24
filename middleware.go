34fe99c1a4bfa4a13ecc5b5b27a8581185c27bc3
Calvin Colton <calvincolton@gmail.com>
2021-12-24 14:15:49 -0800

    CORS preflight

--- middleware.go
+++ middleware.go
@@ -5,6 +5,8 @@ import "net/http"
 func (app *application) enableCORS(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "DELETE, POST, PUT, GET, OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With")
 
 		next.ServeHTTP(w, r)
 	})
